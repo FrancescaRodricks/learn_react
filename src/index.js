ecd2e875fa93931bbbf9f9d0572df0d8621f9a03
Francesca Rodricks <francesca.rodricks@simplybusiness.co.uk>
2019-09-20 14:37:36 +0100

    some examples using JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+// const variable is JSX syntax used to define react elements
+// const helloWorld = <h1>Hello World</h1>;
+
+// const variable = 'hello'
+
+// simple example 1
+// const name = 'francesca rodricks';
+// const element = <h1>hello { name }</h1>;
+
+// example with any valid expression in the curly braces
+
+// const mathOperation = 2+2 ;
+// const element = <h1>{ mathOperation }</h1>
+
+// example with a function
+// const user = {
+//   FirstName: 'Francesca',
+//   LastName: 'Rodricks'
+// }
+
+// function fullName(user)
+// {
+//   return user.FirstName + ' ' + user.LastName
+// }
+
+// const element = fullName(user);
+
+// jsx is an expression too
+
+const user = {
+  FirstName: 'John',
+  LastName: 'Doe'
+};
+
+function getGreeting(user){
+  if(user)
+    return 'Hello' + user.FirstName + ' ' + user.LastName
+  else
+    return 'Hello Stranger'
+}
+
+const element = getGreeting(user);
+
+ReactDOM.render(element, document.getElementById('root'));
 
 serviceWorker.register();
